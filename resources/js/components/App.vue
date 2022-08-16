33cfd7cd3c931865e4e3a6821e6ef42c6c06e579
iDevPrince <prince@intelyticslimited.com>
2022-08-16 18:35:32 +0100

    Update App.vue

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -4,8 +4,8 @@
 		<div class="container-fluid">
 			<router-link to="/" class="nav-brand" href="#">Harde</router-link>
 			<div>
-				<router-link exact-active-class="active" to="/" href="#" class="nav-item nav-link>Home</router-link>
-				<router-link exact-active-class="active" :to="/books" href="#" class="nav-item nav-link">Books</router-link>
+				<router-link exact-active-class="active" to="/" href="#" class="nav-item nav-link">Home</router-link>
+				<router-link exact-active-class="active" to="/books" href="#" class="nav-item nav-link">Books</router-link>
 			</div>
 		</div>
 		</nav>
