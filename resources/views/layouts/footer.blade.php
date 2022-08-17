64258db0c5345fca2881e6dad8819a4920155e5f
Prince-iDev <93903242+Prince-iDev@users.noreply.github.com>
2022-08-17 11:45:02 +0100

    Delete footer.blade.php

--- resources/views/layouts/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- partial:partials/_footer.html -->
-<footer class="footer">
-    <div class="d-sm-flex justify-content-center justify-content-sm-between">
-      <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © Delsea Services 2022. All rights reserved.</span>
-      <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">idevMac - Development<i class="ti-heart text-danger ml-1"></i></span>
-    </div>
-  </footer>
